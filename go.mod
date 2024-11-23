ecaf77d1da24793185bce50e6dcc1e591a6d0311
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-11-23 23:01:07 +0100

    very basic editing

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/maximekuhn/ttedit
 
 go 1.23.2
+
+require (
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,122 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/term"
+)
+
+const (
+	KeyEnter     byte = 13
+	KeySpace     byte = 32
+	KeyBackspace byte = 127
+
+	CRLF             string = "\r\n"
+	ClearScreen      string = "\033[2J"
+	RequestCursorPos string = "\033[6n"
+	SaveCursorPos           = "\033[s"
+	RestoreCursorPos        = "\033[u"
+
+	MoveCursorLeft string = "\033[D"
+	MoveCursorUp   string = "\033[A"
+)
+
+type CursorPos struct {
+	row int
+	col int
+}
+
+func getCursorPos() (CursorPos, error) {
+	fmt.Print(RequestCursorPos)
+	buf := make([]byte, 32)
+	n, err := os.Stdin.Read(buf)
+	if err != nil {
+		return CursorPos{}, err
+	}
+	response := string(buf[:n])
+	if !strings.HasPrefix(response, "\033[") || !strings.HasSuffix(response, "R") {
+		return CursorPos{}, fmt.Errorf("unexpected response format: %s", response)
+	}
+
+	trimmed := strings.TrimSuffix(strings.TrimPrefix(response, "\033["), "R")
+	parts := strings.Split(trimmed, ";")
+	if len(parts) != 2 {
+		return CursorPos{}, fmt.Errorf("unexpected response format: %s", response)
+	}
+
+	row, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return CursorPos{}, err
+	}
+
+	col, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return CursorPos{}, err
+	}
+
+	return CursorPos{
+		row: row,
+		col: col,
+	}, nil
+}
+
+func handleBackspace() error {
+	pos, err := getCursorPos()
+	if err != nil {
+		return err
+	}
+	if pos.col == 1 && pos.row > 1 {
+		fmt.Print(MoveCursorUp)
+
+		// because we don't have a way to know the current line length,
+		// we can't move the cursor to the correct position, which is usually
+		// the last character of the line
+	}
+	fmt.Print(MoveCursorLeft)
+	fmt.Printf("%c", KeySpace)
+	fmt.Print(MoveCursorLeft)
+	return nil
+
+}
 
 func main() {
-	fmt.Println("hello ttedit")
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	fmt.Printf(ClearScreen)
+	fmt.Printf("\033[0;0H")
+
+	for {
+		buf := make([]byte, 1)
+		_, err = os.Stdin.Read(buf)
+		if err != nil {
+			fmt.Printf("stdin.Read(): %v\n", err)
+			return
+		}
+
+		input := buf[0]
+		if input == 'q' {
+			return
+		}
+
+		if input == KeyEnter {
+			fmt.Print(CRLF)
+			continue
+		}
+
+		if input == KeyBackspace {
+			if err = handleBackspace(); err != nil {
+				return
+			}
+			continue
+		}
+
+		fmt.Printf("%c", input)
+	}
 }
